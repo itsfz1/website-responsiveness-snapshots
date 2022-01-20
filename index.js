c253d37e114a3fd4ad32c2e61ae021cc1a176b4d
fahimzada <fahim.neslit@gmail.com>
2022-01-20 05:15:57 +0500

    Deployment

--- index.js
+++ index.js
@@ -8,50 +8,50 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
-const mediaLocation = "./public/images";
+const mediaLocation = "/public/images";
 
 const captureScreenshot = async (link, device) => {
   console.log("Inside screenshoot");
-  const domainName = link.slice(link.indexOf(".") + 1, link.lastIndexOf("."));
-  const customDevices = {
-    xl: {
-      width: 1280,
-      height: 600,
-    },
-    xxl: {
-      width: 1536,
-      height: 1000,
-    },
-  };
-  const image = `${mediaLocation}/${domainName}_${device}_${Date.now()}.png`;
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(60000);
-  if (customDevices.hasOwnProperty(device)) {
-    await page.setViewport({
-      width: customDevices[device].width,
-      height: customDevices[device].height,
-      deviceScaleFactor: 2,
-    });
-  } else {
-    await page.emulate(puppeteer.devices[device]);
-  }
   try {
+    const domainName = link.slice(link.indexOf(".") + 1, link.lastIndexOf("."));
+    const customDevices = {
+      xl: {
+        width: 1280,
+        height: 600,
+      },
+      xxl: {
+        width: 1536,
+        height: 1000,
+      },
+    };
+    const image = `${mediaLocation}/${domainName}_${device}_${Date.now()}.png`;
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(60000);
+    if (customDevices.hasOwnProperty(device)) {
+      await page.setViewport({
+        width: customDevices[device].width,
+        height: customDevices[device].height,
+        deviceScaleFactor: 2,
+      });
+    } else {
+      await page.emulate(puppeteer.devices[device]);
+    }
     await page.goto(link, { waitUntil: "networkidle2" });
-  } catch (e) {
-    console.log(e);
-  }
-  await page.waitForTimeout(7000);
-  await page.screenshot({
-    path: `${image}`,
-    fullPage: true,
-  });
+    await page.waitForTimeout(7000);
+    await page.screenshot({
+      path: `${image}`,
+      fullPage: true,
+    });
 
-  await browser.close();
-  return image;
+    await browser.close();
+    return image;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 app.get("/", (req, res) => {
