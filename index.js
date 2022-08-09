4d8f04709a4948a5a86f64941505379af608a9fd
itsfz1 <programmingway1@gmail.com>
2022-08-09 23:13:35 +0500

    console logs removed

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-const PORT = process.env.PORT || 3000;
-const path = require("path");
+const express = require("express")
+const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 3000
+const path = require("path")
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")))
 
-const puppeteer = require("puppeteer");
-const fs = require("fs/promises");
-//const mediaLocation = "/public";
+const puppeteer = require("puppeteer")
 
 const captureScreenshot = async (link, device) => {
-  console.log("Inside screenshoot");
   try {
-    let domainName = link.slice(link.indexOf(".") + 1, link.lastIndexOf("."));
+    let domainName = link.slice(link.indexOf(".") + 1, link.lastIndexOf("."))
     const customDevices = {
       xl: {
         width: 1280,
@@ -23,62 +20,55 @@ const captureScreenshot = async (link, device) => {
         width: 1536,
         height: 1000,
       },
-    };
-    const image = `${domainName}_${device}_${Date.now()}.png`;
+    }
+    const image = `${domainName}_${device}_${Date.now()}.png`
     const browser = await puppeteer.launch({
       headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--single-process",
-      ],
-    });
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(60000);
+      args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--single-process"],
+    })
+    const page = await browser.newPage()
+    await page.setDefaultNavigationTimeout(60000)
     if (customDevices.hasOwnProperty(device)) {
       await page.setViewport({
         width: customDevices[device].width,
         height: customDevices[device].height,
         deviceScaleFactor: 2,
-      });
+      })
     } else {
-      await page.emulate(puppeteer.devices[device]);
+      await page.emulate(puppeteer.devices[device])
     }
-    await page.goto(link, { waitUntil: "networkidle2" });
-    await page.waitForTimeout(7000);
+    await page.goto(link, { waitUntil: "networkidle2" })
+    await page.waitForTimeout(7000)
     await page.screenshot({
       path: `${image}`,
       fullPage: true,
-    });
+    })
 
-    await browser.close();
-    return image;
+    await browser.close()
+    return image
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+  res.sendFile(path.join(__dirname, "index.html"))
+})
 
 app.post("/mysnapshot", async (req, res) => {
-  const { link, device } = req.body;
-  console.log(req.body);
-  let image = "";
-  if (link !== undefined || link === "") {
-    image = await captureScreenshot(link, device);
-    const filename = path.basename(image);
-    console.log(filename, "filename");
-    res.setHeader("Content-disposition", "attachment; filename=" + filename);
-    res.setHeader("content-type", "image/png");
-    res.download(image, filename);
+  const { link, device } = req.body
+  let image = ""
+  if (link !== undefined && link === "") {
+    image = await captureScreenshot(link, device)
+    const filename = path.basename(image)
+    res.setHeader("Content-disposition", "attachment; filename=" + filename)
+    res.setHeader("content-type", "image/png")
+    res.download(image, filename)
   } else {
-    res.send({ error: "Please provide a valid url" });
+    res.send({ error: "Please provide a valid url" })
   }
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+  console.log(`Listening on port ${PORT}`)
+})
